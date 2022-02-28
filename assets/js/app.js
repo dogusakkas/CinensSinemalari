8776175b2442ef542d100bd1aaae7b42dab8cc6a
dogusakkas <dogusakkas@gmail.com>
2022-02-28 18:31:21 +0300

    film sayfasının tasarımı

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/* ŞUBELERİMİZ */
+/* Dropdown */
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -17,7 +17,28 @@ window.onclick = function(event) {
     }
 }
 
-/* ŞUBELERİMİZ */
+/* Dropdown */
+
+/* Movies Dropdown */
+
+function myFunction2() {
+    document.getElementById("moviesDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn2')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content2");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+/* Movies Dropdown */
 
 /* POP-UP */
 
