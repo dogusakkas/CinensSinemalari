0602884b6b7650df9961d20fd7d5e9e49111cd30
dogusakkas <dogusakkas@gmail.com>
2022-03-02 00:09:57 +0300

    Film sayfasında tablinks bölümü

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,5 +1,5 @@
 //Tab Content
-
+//index.html
 function sections(evt, scts) {
     var i, tabcontent, tablinks;
 
@@ -14,5 +14,22 @@ function sections(evt, scts) {
     }
 
     document.getElementById(scts).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+//movies.html
+
+
+function sessions(evt, date) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(date).style.display = "block";
     evt.currentTarget.className += " active";
 }
